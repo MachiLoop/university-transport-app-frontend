85f4256ba00c24c01f5a87c8abdc490aaaf1c597
machiloop <hamidusodiq2@gmail.com>
2025-03-15 01:03:48 +0100

    set up app boilerplate

---
+++ constants/icons.js
@@ -0,0 +1,106 @@
+import add from "../assets/icons/add.png";
+import battery from "../assets/icons/battery.png";
+import bell from "../assets/icons/bell.png";
+import camera from "../assets/icons/camera.png";
+import camera2 from "../assets/icons/camera2.png";
+import checkbox from "../assets/icons/checkbox.png";
+import checkmark from "../assets/icons/checkmark.png";
+import copy from "../assets/icons/copy.png";
+import discount from "../assets/icons/discount.png";
+import discountFilled from "../assets/icons/discountFilled.png";
+import downArrow from "../assets/icons/downArrow.png";
+import downLeft from "../assets/icons/downLeft.png";
+import eye from "../assets/icons/eye.png";
+import eyeHide from "../assets/icons/eyeHide.png";
+import file from "../assets/icons/file.png";
+import fuel from "../assets/icons/fuel.png";
+import heart from "../assets/icons/heart.png";
+import home from "../assets/icons/home.png";
+import homeFilled from "../assets/icons/homeFilled.png";
+import leftArrow from "../assets/icons/leftArrow.png";
+import like from "../assets/icons/like.png";
+import logout from "../assets/icons/logout.png";
+import map from "../assets/icons/map.png";
+import mapFilled from "../assets/icons/mapFilled.png";
+import minus from "../assets/icons/minus.png";
+import mph from "../assets/icons/mph.png";
+import mute from "../assets/icons/mute.png";
+import onButton from "../assets/icons/onButton.png";
+import options from "../assets/icons/options.png";
+import precision from "../assets/icons/precision.png";
+import precision2 from "../assets/icons/precision2.png";
+import question from "../assets/icons/question.png";
+import rightArrow from "../assets/icons/rightArrow.png";
+import search from "../assets/icons/search.png";
+import send from "../assets/icons/send.png";
+import settings from "../assets/icons/settings.png";
+import smile from "../assets/icons/smile.png";
+import speedometer from "../assets/icons/speedometer.png";
+import sync from "../assets/icons/sync.png";
+import team from "../assets/icons/team.png";
+import telephone from "../assets/icons/telephone.png";
+import upArrow from "../assets/icons/upArrow.png";
+import upRightArrow from "../assets/icons/upRightArrow.png";
+import user from "../assets/icons/user.png";
+import userFilled from "../assets/icons/userFilled.png";
+import warning from "../assets/icons/warning.png";
+import warning2 from "../assets/icons/warning2.png";
+import wallet from "../assets/icons/wallet.png";
+import walletFilled from "../assets/icons/walletFilled.png";
+import x from "../assets/icons/x.png";
+import videoCamera from "../assets/icons/videoCamera.png";
+
+export default {
+  add,
+  battery,
+  bell,
+  camera,
+  profile,
+  camera2,
+  checkbox,
+  checkmark,
+  copy,
+  discount,
+  discountFilled,
+  downArrow,
+  downLeft,
+  eye,
+  eyeHide,
+  file,
+  fuel,
+  heart,
+  home,
+  homeFilled,
+  leftArrow,
+  like,
+  logout,
+  map,
+  mapFilled,
+  minus,
+  mph,
+  mute,
+  onButton,
+  options,
+  precision,
+  precision2,
+  question,
+  rightArrow,
+  search,
+  send,
+  settings,
+  smile,
+  speedometer,
+  sync,
+  team,
+  telephone,
+  upArrow,
+  upRightArrow,
+  user,
+  userFilled,
+  videoCamera,
+  wallet,
+  walletFilled,
+  warning,
+  warning2,
+  x,
+};
