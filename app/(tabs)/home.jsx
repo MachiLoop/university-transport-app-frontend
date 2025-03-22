0a22d79ad4136941878e5b7f67d7f2cde796c2f5
machiloop <hamidusodiq2@gmail.com>
2025-03-22 12:47:22 +0100

    feat: implement location picker dropdown component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,32 @@
 import { View, Text } from "react-native";
-import React from "react";
+import DropdownComponent from "../../components/dropdownComponent";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [destinationLocation, setDestinationLocation] = useState(null);
+
+  useEffect(() => {
+    console.log(currentLocation);
+    console.log(destinationLocation);
+  }, [currentLocation, destinationLocation]);
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <DropdownComponent
+        value={currentLocation}
+        setValue={setCurrentLocation}
+        label="Current location"
+        placeholder="Where are you?"
+      />
+      <DropdownComponent
+        value={destinationLocation}
+        setValue={setDestinationLocation}
+        label="Destination location"
+        placeholder="Where do you want to go?"
+      />
+    </SafeAreaView>
   );
 };
 
---
+++ components/dropdownComponent.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import locations from "../data/locations";
+import { icons } from "../constants";
+
+const DropdownComponent = ({ value, setValue, label, placeholder }) => {
+  const [isFocus, setIsFocus] = useState(false);
+
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return (
+        <Text style={[styles.label, isFocus && { color: "blue" }]}>
+          {label}
+        </Text>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderLabel()}
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={locations}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? placeholder : "..."}
+        searchPlaceholder="Search..."
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+        renderLeftIcon={() => (
+          <Image
+            source={icons.mapFilled}
+            style={styles.icon}
+            className="w-5 h-5"
+            tintColor={isFocus ? "#E23859" : "#08B783"}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+export default DropdownComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+    color: "#5A5A5A",
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    color: "#5A5A5A",
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    color: "#414141",
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
